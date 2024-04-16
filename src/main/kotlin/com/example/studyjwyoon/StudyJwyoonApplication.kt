2e470ecdbff6b89e5356edc4e20501cd232a0841
윤재욱_디스커버리개발팀 <jw.yoon8@oliveyoung.co.kr>
2024-04-16 12:12:42 +0900

    start

---
+++ src/main/kotlin/com/example/studyjwyoon/StudyJwyoonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studyjwyoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyJwyoonApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyJwyoonApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/studyjwyoon/StudyJwyoonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.studyjwyoon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyJwyoonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
